e8a0ca21fc080c7d6ef9edd52cb0105add5721f9
Deva658 <devaaditya271@gmail.com>
2025-05-22 10:48:02 +0700

    merapihkan codingan

--- file1.cpp
+++ file1.cpp
@@ -3,46 +3,53 @@
 using namespace std;
 
 // node class representing a single node in the linked list
-class Node {
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node() {
+    Node()
+    {
         next = NULL;
     }
 };
 
 // stack class
-class Stack {
+class Stack
+{
 private:
- Node* top; // pointer to the top node of the stack
+    Node *top; // pointer to the top node of the stack
 
 public:
- Stack() {
-  top = NULL; // intialaze the stack with a null top pointer
- }
+    Stack()
+    {
+        top = NULL; // intialaze the stack with a null top pointer
+    }
 
- // push operation: insert an element onte the top of the stack
- int push(int value) {
-  Node* newNode = new Node(); // 1.allocate mmeory for the new node
-  newNode->data = value; // 2.assign value
-  newNode->next = top; // 3. set the next pointer of the new node to the curret top
-  top = newNode; // 4, update the top pointer to the new node
-  cout << "Push Value: " << value << endl;
-  return;
- }
+    // push operation: insert an element onte the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1.allocate mmeory for the new node
+        newNode->data = value;      // 2.assign value
+        newNode->next = top;        // 3. set the next pointer of the new node to the curret top
+        top = newNode;              // 4, update the top pointer to the new node
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
 
- // pop operation: remove the topmodt elemenet from the stack
-  void pop() {
-   if (isEmpty()) {
-    cout << "stack is empty. " << endl;
-   }
+    // pop operation: remove the topmodt elemenet from the stack
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "stack is empty. " << endl;
+        }
 
-   Node* temp = top; // create a temporary pointer to the nods
-   top = top->next; // update the top pointer to the next node
-   cout << "Popped value: " << top->data << endl;
-  }
+        Node *temp = top; // create a temporary pointer to the nods
+        top = top->next;  // update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+    }
 
     // peak/top operation: retrieve the value of topmost element without removig
     void peak()
@@ -63,7 +70,7 @@ public:
         } // return the value of the top node
     }
 
-    //isempty operation: check if the stack is empty
+    // isempty operation: check if the stack is empty
     bool isEmpty()
     {
         return top == NULL; // returm true if the top pointer s null, indicating
@@ -93,13 +100,14 @@ int main()
             cin >> value;
             stack.push(value); // push the entered value onto the stack
             break;
-        
+
         case 2:
             if (!stack.isEmpty())
             {
                 stack.pop(); // pop the top element from the stack
             }
-            else;
+            else
+                ;
             {
                 cout << "Stack is empty. cannot pop." << endl;
             }
