70934db1f82b447f9ea7accc5f7eed48a8226abd
Deva658 <devaaditya271@gmail.com>
2025-05-22 10:30:09 +0700

    Implementasi case push dalam switch menu di main

--- file1.cpp
+++ file1.cpp
@@ -85,5 +85,14 @@ int main()
         cout << "4. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value); // push the entered value onto the stack
+            break;
+        }
     }
 }
