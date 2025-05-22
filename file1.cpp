31ceefed3b8d4bb40ca5b6795acb543816b0679a
Deva658 <devaaditya271@gmail.com>
2025-05-22 09:51:05 +0700

    membuat class stack

--- file1.cpp
+++ file1.cpp
@@ -12,3 +12,14 @@ public:
         next = NULL;
     }
 };
+
+// stack class
+class Stack {
+private:
+ Node* top; // pointer to the top node of the stack
+
+public:
+ Stack() {
+  top = NULL; // intialaze the stack with a null top pointer
+ }
+}
